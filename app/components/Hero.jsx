a2447fc686c65b95374460bfbee2e2e4092c8d8f
tahirasif31 <tahirasif31@gmail.com>
2024-02-21 21:37:55 +0500

    hero image removed from mobile devices

--- app/components/Hero.jsx
+++ app/components/Hero.jsx
@@ -41,6 +41,7 @@ function Hero() {
         </div>
         <div className="imgContainer">
           <Image
+            className={styles.heroImage}
             src={"/HeroImg.svg"}
             alt="boy and girl chating on whatsapp"
             width={546}
