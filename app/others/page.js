f8d90d1b2161f44803c2528af9e82156db181970
tahirasif31 <tahirasif31@gmail.com>
2024-02-22 16:07:06 +0500

    fun page completed

--- app/others/page.js
+++ app/others/page.js
@@ -1,26 +1,25 @@
-"use client";
 import Heading from "../components/Heading";
-import LinkHeader from "../components/LinkHeader";
-import LinkRows from "../components/LinkRows";
-import LoadingSpinner from "../components/LoadingSpinner";
-import { useFetchGroups } from "../hooks/useFetchGroups";
+import Fetcher from "../components/Fetcher";
 
-function Others() {
-  const [groups, isLoading] = useFetchGroups("other");
-
-  if (isLoading) return <LoadingSpinner />;
-
+function Girls() {
   return (
     <main className="container">
-      <Heading title={"Other"} />
-      <section className="content-container">
-        <LinkHeader />
-        {groups.map((group) => (
-          <LinkRows group={group} key={group} />
-        ))}
-      </section>
+      <Heading title={"Fun Groups"} end={"Entertainment"} />
+      <article className="content-article">
+        Welcome to the ultimate hub of fun and entertainment! Dive into a world
+        of laughter, excitement, and endless amusement by joining our exclusive
+        WhatsApp group. Discover a diverse community of fun-seekers where every
+        moment is packed with joy and camaraderie. Whether you&apos;re
+        passionate about sharing hilarious memes, engaging in lively debates, or
+        simply connecting with fellow enthusiasts, you&apos;ll find your perfect
+        match here. Join us today and unlock a treasure trove of unforgettable
+        experiences. Let&apos;s create memories, share laughter, and make every
+        moment count together! Don&apos;t miss out &mdash; join our
+        fun&mdash;filled WhatsApp group now!
+      </article>
+      <Fetcher title={"others"} />
     </main>
   );
 }
 
-export default Others;
+export default Girls;
