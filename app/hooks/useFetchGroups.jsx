a27d3ccf7347cc877a8063965d22767c8b8f2443
tahirasif31 <tahirasif31@gmail.com>
2024-02-22 14:15:38 +0500

    build bugs fixed

--- app/hooks/useFetchGroups.jsx
+++ app/hooks/useFetchGroups.jsx
@@ -6,20 +6,23 @@ function useFetchGroups(category) {
   const [groups, setGroups] = useState([]);
   const [isLoading, setIsLoading] = useState(false);
 
-  useEffect(function () {
-    async function getGroups() {
-      setIsLoading(true);
-      const { data: groups, error } = await supabase
-        .from("groups")
-        .select("*")
-        .eq("category", category);
-      console.log(groups);
-      if (error) alert("There is a problem while loading your groups");
-      if (!error) setGroups(groups);
-      setIsLoading(false);
-    }
-    getGroups();
-  }, []);
+  useEffect(
+    function () {
+      async function getGroups() {
+        setIsLoading(true);
+        const { data: groups, error } = await supabase
+          .from("groups")
+          .select("*")
+          .eq("category", category);
+        console.log(groups);
+        if (error) alert("There is a problem while loading your groups");
+        if (!error) setGroups(groups);
+        setIsLoading(false);
+      }
+      getGroups();
+    },
+    [category]
+  );
 
   return [groups, isLoading];
 }
--- app/news/page.js
+++ app/news/page.js
@@ -11,12 +11,13 @@ function News() {
         enthusiasts to receive real-time updates on the latest headlines,
         trends, and stories shaping the world. From breaking news alerts to
         in-depth analysis and thought-provoking discussions, our WhatsApp groups
-        delivers curated content straight to your device, ensuring you're always
-        ahead of the curve. Whether you're passionate about politics, business,
-        technology, entertainment, or sports, our group covers it all, fostering
-        a vibrant space for sharing knowledge and insights. Don't miss out on
-        being part of this engaging community—Join now and experience the
-        convenience of staying informed right at your fingertips.
+        delivers curated content straight to your device, ensuring you&apos;re
+        always ahead of the curve. Whether you&apos;re passionate about
+        politics, business, technology, entertainment, or sports, our group
+        covers it all, fostering a vibrant space for sharing knowledge and
+        insights. Don&apos;t miss out on being part of this engaging
+        community—Join now and experience the convenience of staying informed
+        right at your fingertips.
       </article>
       <Fetcher title={"news"} />
     </main>
